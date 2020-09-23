59648082a9ac05c5911ca803e5332c481387046d
sofie-rk <sofierk@stud.ntnu.no>
2020-09-23 11:08:15 +0200

    first attempt assignment 5

--- assignment_5/script.js
+++ assignment_5/script.js
@@ -1,23 +1,61 @@
 /* Part 2 */
 console.log('PART 2')
+// For-loop, prints integers from 1 to 20 in developer console
+for (let i = 1; i < 21; i++) {
+    console.log(i)
+}
 
 /* Part 3 */
 console.log('PART 3')
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]%3 == 0 && numbers[i]%5 == 0) {
+        console.log("eplekake")
+    } else if (numbers[i]%3 == 0) {
+        console.log("eple")
+    } else if (numbers[i]%5 == 0) {
+        console.log("kake")
+    }
+}
+
 /* Part 4 */
 
-/* Part 5 */
-function changeDisplay () {
+// returns the element that has the ID attribute "title"
+let title = document.getElementById("title")
+// Set the text content of the title
+title.innerText = "Hello JavaScript"
 
+/* Part 5 */
+
+// Creating a variable since this element is used in many functions later
+let magic = document.getElementById("magic")
+
+// Adds event handling
+// Example: document.getElementById("none") gets the button with id="none"
+// .addEventListener("click", changeDisplay) when button is clicked, and event is occuring running function changeDisplay
+
+document.getElementById("none").addEventListener("click", changeDisplay)
+document.getElementById("hidden").addEventListener("click", changeVisibility)
+document.getElementById("reset").addEventListener("click", reset)
+
+function changeDisplay () {
+    // div with id=magic is removed from the document flow
+    magic.style.display = "none"
 }
 
 function changeVisibility () {
-
+    // div with id=magic is invisible, but takes up place in the document flow
+    magic.style.visibility = "hidden"
+    magic.style.display = "block"
+    
 }
 
 function reset () {
+    // The defualt values, visibility: visible, display: block
+    magic.style.visibility = "visible"
+    magic.style.display = "block"
 
 }
 
@@ -35,3 +73,13 @@ const technologies = [
     'Bootstrap',
     'Node.js'
 ];
+
+for (let i = 0; i < technologies.length; i++) {
+    var node = document.createElement("LI")                 // create <li> node
+    var textnode = document.createTextNode(technologies[i]) // create text node
+    node.appendChild(textnode)                              // append the text to <li>
+    document.getElementById("tech").appendChild(node)       // append <li> to <ul> with id="tech"
+}
+
+
+
